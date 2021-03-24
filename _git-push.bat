e0223dd5d07d0e088cac23d89a756a238380960f
Chase Hall <c@chse.xyz>
2021-03-23 20:25:31 -0500

    Updated Things

--- _git-push.bat
+++ _git-push.bat
@@ -4,8 +4,8 @@
 
 del /s /q iw4x.stat
 RD /S /Q demos
-xcopy . %USERPROFILE%\Documents\GitHub\IW4x_Promod\ /S /Q /Y
-cd %USERPROFILE%\Documents\GitHub\IW4x_Promod\
+xcopy . %USERPROFILE%\Documents\GitHub\IW4x-Promod\ /S /Q /Y
+cd %USERPROFILE%\Documents\GitHub\IW4x-Promod\
 git add *
 git commit -a -m "Updated Things"
 git push
